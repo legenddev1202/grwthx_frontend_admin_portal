14d5a1701aeaa0473011ee590258b1e7382279e9
grwthx <grwthx@grwth.hk>
2023-10-29 00:46:38 -0700

    build for preview dir change

--- src/views/openDay/beehiveData.js
+++ src/views/openDay/beehiveData.js
@@ -1,223 +1,223 @@
 const beehiveData = [
     {
         imgUrl: '../images/01.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=fg53xecEYHb4wi6bDv82WG28lVGaSK&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=fg53xecEYHb4wi6bDv82WG28lVGaSK&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
     },
     {
         imgUrl: '../images/02.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=WqvCgCfK5w0mml5Z66pgtzRE4lFncn&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=WqvCgCfK5w0mml5Z66pgtzRE4lFncn&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
     },
     {
         imgUrl: '../images/03.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=n6NCMUUawDJLn3LnZ78eRc5jp8yN0V&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=n6NCMUUawDJLn3LnZ78eRc5jp8yN0V&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
     },
     {
         imgUrl: '../images/04.png',
-        playUrl: ' https://play.grwth.hk/play/?roomId=RyJEaNT5edOZKglItDL0assUfe5s9n&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
+        playUrl: ' https://play.grwth.hk/preview/?roomId=RyJEaNT5edOZKglItDL0assUfe5s9n&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
     },
     {
         imgUrl: '../images/05.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=ZeNQa8JAqIbFidOOVPueJ5GFYruAoM&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=ZeNQa8JAqIbFidOOVPueJ5GFYruAoM&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
     },
     {
         imgUrl: '../images/06.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=mU19smzuFhg83tp2Ks4RbWzqXt1XiX&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=mU19smzuFhg83tp2Ks4RbWzqXt1XiX&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
     },
     {
         imgUrl: '../images/07.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=SwMIfgnbJ6OGA3ukhGm2HLrJYmryhj&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=SwMIfgnbJ6OGA3ukhGm2HLrJYmryhj&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
     },
     {
         imgUrl: '../images/08.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=44DWIasRgZk0W0QYE1w8JlNf0s0QyP&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=44DWIasRgZk0W0QYE1w8JlNf0s0QyP&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
     },
     {
         imgUrl: '../images/09.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=ReninpUuU3HWQE6XAI1KUnT6O2VoXz&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=ReninpUuU3HWQE6XAI1KUnT6O2VoXz&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
     },
     {
         imgUrl: '../images/10.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=sK2zwufgRD6UZ8GWID1nk356ww2lm4&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=sK2zwufgRD6UZ8GWID1nk356ww2lm4&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
     },
     {
         imgUrl: '../images/11.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=sK2zwufgRD6UZ8GWID1nk356ww2lm4&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=sK2zwufgRD6UZ8GWID1nk356ww2lm4&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
     },
     // {
     //     imgUrl: '../images/12.png',
-    //     playUrl: 'https://play.grwth.hk/play/?roomId=gpYLkapAxY04gJzTM3nUEZWw0AmWA3&userId=27cb7a613d99e87f8e0ac29d4027ced3&assignmentId=undefined'
+    //     playUrl: 'https://play.grwth.hk/preview/?roomId=gpYLkapAxY04gJzTM3nUEZWw0AmWA3&userId=27cb7a613d99e87f8e0ac29d4027ced3&assignmentId=undefined'
     // },
     {
         imgUrl: '../images/13.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=5CDTPENwkfzF3mHg0yQhTScutHgYIW&userId=27cb7a613d99e87f8e0ac29d4027ced3&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=5CDTPENwkfzF3mHg0yQhTScutHgYIW&userId=27cb7a613d99e87f8e0ac29d4027ced3&assignmentId=undefined'
     },
     {
         imgUrl: '../images/14.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=Mzaj6mHQDKp6lTmQ6bGXikhTi7503r&userId=27cb7a613d99e87f8e0ac29d4027ced3&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=Mzaj6mHQDKp6lTmQ6bGXikhTi7503r&userId=27cb7a613d99e87f8e0ac29d4027ced3&assignmentId=undefined'
     },
     {
         imgUrl: '../images/15.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=YYqOIdosHOIjLQsjPzNIP5JqHMiUID&userId=27cb7a613d99e87f8e0ac29d4027ced3&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=YYqOIdosHOIjLQsjPzNIP5JqHMiUID&userId=27cb7a613d99e87f8e0ac29d4027ced3&assignmentId=undefined'
     },
     {
         imgUrl: '../images/16.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=qP0EMPFFWJZ6Sbdycvqp5PQehmq3yP&userId=27cb7a613d99e87f8e0ac29d4027ced3&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=qP0EMPFFWJZ6Sbdycvqp5PQehmq3yP&userId=27cb7a613d99e87f8e0ac29d4027ced3&assignmentId=undefined'
     },
     {
         imgUrl: '../images/17.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=loVNGanm0b2RAmVLGcVTveq4CfU2DD&userId=27cb7a613d99e87f8e0ac29d4027ced3&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=loVNGanm0b2RAmVLGcVTveq4CfU2DD&userId=27cb7a613d99e87f8e0ac29d4027ced3&assignmentId=undefined'
     },
     {
         imgUrl: '../images/18.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=1m6XiZUpwEaWCVYT0RdgP3OTPHBxpR&userId=27cb7a613d99e87f8e0ac29d4027ced3&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=1m6XiZUpwEaWCVYT0RdgP3OTPHBxpR&userId=27cb7a613d99e87f8e0ac29d4027ced3&assignmentId=undefined'
     },
     // {
     //     imgUrl: '../images/19.png',
-    //     playUrl: 'https://play.grwth.hk/play/?roomId=1m6XiZUpwEaWCVYT0RdgP3OTPHBxpR&userId=27cb7a613d99e87f8e0ac29d4027ced3&assignmentId=undefined'
+    //     playUrl: 'https://play.grwth.hk/preview/?roomId=1m6XiZUpwEaWCVYT0RdgP3OTPHBxpR&userId=27cb7a613d99e87f8e0ac29d4027ced3&assignmentId=undefined'
     // },
     {
         imgUrl: '../images/20.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=aKuMf3FajLqhbchOQ6VwHHFyQIvkt2&userId=27cb7a613d99e87f8e0ac29d4027ced3&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=aKuMf3FajLqhbchOQ6VwHHFyQIvkt2&userId=27cb7a613d99e87f8e0ac29d4027ced3&assignmentId=undefined'
     },
     {
         imgUrl: '../images/21.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=4JJrf9RklxGp4yFVH98ZhMFO6s5qHD&userId=27cb7a613d99e87f4ce6922ad4dc7a52&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=4JJrf9RklxGp4yFVH98ZhMFO6s5qHD&userId=27cb7a613d99e87f4ce6922ad4dc7a52&assignmentId=undefined'
     },
     {
         imgUrl: '../images/22.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=8MiLOAaiQboGZtkBI4sl0Ndhql4iKw&userId=27cb7a613d99e87fbee0fd3866ff914b'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=8MiLOAaiQboGZtkBI4sl0Ndhql4iKw&userId=27cb7a613d99e87fbee0fd3866ff914b'
     },
     {
         imgUrl: '../images/24.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=GadkLXtmx8LAj4Xa4W3UPUVbg6FBsw&userId=27cb7a613d99e87fbee0fd3866ff914b'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=GadkLXtmx8LAj4Xa4W3UPUVbg6FBsw&userId=27cb7a613d99e87fbee0fd3866ff914b'
     },
     {
         imgUrl: '../images/25.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=PValFEWtyB8FaXw7Oikni1Rsd1xczZ&userId=27cb7a613d99e87fbee0fd3866ff914b'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=PValFEWtyB8FaXw7Oikni1Rsd1xczZ&userId=27cb7a613d99e87fbee0fd3866ff914b'
     },
     {
         imgUrl: '../images/26.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=3l1twxkTFicyqcczmbzCWNMwzdPTzg&userId=27cb7a613d99e87fbee0fd3866ff914b'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=3l1twxkTFicyqcczmbzCWNMwzdPTzg&userId=27cb7a613d99e87fbee0fd3866ff914b'
     },
     {
         imgUrl: '../images/27.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=bmJ12TX2DEm2zADz4P48p8IbnzSNaN&amp;userId=27cb7a613d99e87f4ce6922ad4dc7a52&amp;assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=bmJ12TX2DEm2zADz4P48p8IbnzSNaN&amp;userId=27cb7a613d99e87f4ce6922ad4dc7a52&amp;assignmentId=undefined'
     },
     {
         imgUrl: '../images/28.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=3DbwbPtCPoQbKWQMkdrGQsCfUynNXo&userId=27cb7a613d99e87fbee0fd3866ff914b'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=3DbwbPtCPoQbKWQMkdrGQsCfUynNXo&userId=27cb7a613d99e87fbee0fd3866ff914b'
     },
     {
         imgUrl: '../images/29.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=2FTBx3hJps8G3GUERW7UaT0NSQJAMt&userId=27cb7a613d99e87fbee0fd3866ff914b'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=2FTBx3hJps8G3GUERW7UaT0NSQJAMt&userId=27cb7a613d99e87fbee0fd3866ff914b'
     },
     {
         imgUrl: '../images/30.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=QMEb0mzMnmeNIchyUAiIt82pUKuEQS&userId=27cb7a613d99e87fbee0fd3866ff914b'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=QMEb0mzMnmeNIchyUAiIt82pUKuEQS&userId=27cb7a613d99e87fbee0fd3866ff914b'
     },
     {
         imgUrl: '../images/31.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=zk12lWeQrL0iyZVzth65mhWUCYfubc&userId=27cb7a613d99e87f998183af8de6c415'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=zk12lWeQrL0iyZVzth65mhWUCYfubc&userId=27cb7a613d99e87f998183af8de6c415'
     },
     {
         imgUrl: '../images/32.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=YpwXuOTWjscAfpAQdYZd6rSL0Vv68Z&userId=27cb7a613d99e87f998183af8de6c415&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=YpwXuOTWjscAfpAQdYZd6rSL0Vv68Z&userId=27cb7a613d99e87f998183af8de6c415&assignmentId=undefined'
     },
     {
         imgUrl: '../images/33.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=4xOcZoBFkzuQzodEZpv5qwJ6nH6Ox0&userId=27cb7a613d99e87f998183af8de6c415'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=4xOcZoBFkzuQzodEZpv5qwJ6nH6Ox0&userId=27cb7a613d99e87f998183af8de6c415'
     },
     {
         imgUrl: '../images/34.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=b71geNNMzcTv1qOmL9I7d8Ms648bYQ&userId=27cb7a613d99e87f998183af8de6c415'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=b71geNNMzcTv1qOmL9I7d8Ms648bYQ&userId=27cb7a613d99e87f998183af8de6c415'
     },
     {
         imgUrl: '../images/35.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=oLuBXE8ShXTsLvgADNmk0V5mX19iNE&userId=27cb7a613d99e87f998183af8de6c415'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=oLuBXE8ShXTsLvgADNmk0V5mX19iNE&userId=27cb7a613d99e87f998183af8de6c415'
     },
     {
         imgUrl: '../images/36.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=qecgSDMzE3NsynrMg4iPl8wy4xNyXt&userId=27cb7a613d99e87f998183af8de6c415'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=qecgSDMzE3NsynrMg4iPl8wy4xNyXt&userId=27cb7a613d99e87f998183af8de6c415'
     },
     {
         imgUrl: '../images/37.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=4J5UgbWugOF1UmGmQUGvzYBNCT557B&userId=27cb7a613d99e87f998183af8de6c415'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=4J5UgbWugOF1UmGmQUGvzYBNCT557B&userId=27cb7a613d99e87f998183af8de6c415'
     },
     {
         imgUrl: '../images/38.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=uBLPkOTXEbGsZj6tqFiVq43nXqZd95&userId=27cb7a613d99e87f998183af8de6c415'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=uBLPkOTXEbGsZj6tqFiVq43nXqZd95&userId=27cb7a613d99e87f998183af8de6c415'
     },
     {
         imgUrl: '../images/39.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=3FBhRMsKX8F62hpuzW1zxhloCvU8CZ&userId=27cb7a613d99e87f998183af8de6c415'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=3FBhRMsKX8F62hpuzW1zxhloCvU8CZ&userId=27cb7a613d99e87f998183af8de6c415'
     },
     {
         imgUrl: '../images/40.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=vDyIcY7llKS9vbdHuHMUExhbysjNW0&userId=27cb7a613d99e87f998183af8de6c415'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=vDyIcY7llKS9vbdHuHMUExhbysjNW0&userId=27cb7a613d99e87f998183af8de6c415'
     },
     // {
     //     imgUrl: '../images/41.png',
-    //     playUrl: 'https://play.grwth.hk/play/?roomId=J9X8xpRWvPP8x8p00vRFOSzi8qcSx3&userId=27cb7a613d99e87f998183af8de6c415'
+    //     playUrl: 'https://play.grwth.hk/preview/?roomId=J9X8xpRWvPP8x8p00vRFOSzi8qcSx3&userId=27cb7a613d99e87f998183af8de6c415'
     // },
     {
         imgUrl: '../images/42.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=6M6WeTb93GNRannLQXqtonLfFQQiFz&userId=27cb7a613d99e87f998183af8de6c415'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=6M6WeTb93GNRannLQXqtonLfFQQiFz&userId=27cb7a613d99e87f998183af8de6c415'
     },
     {
         imgUrl: '../images/43.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=ZqH7DKZrfpp51DxZaQEnd7naddZpbk&userId=27cb7a613d99e87f4ce6922ad4dc7a52&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=ZqH7DKZrfpp51DxZaQEnd7naddZpbk&userId=27cb7a613d99e87f4ce6922ad4dc7a52&assignmentId=undefined'
     },
     // {
     //     imgUrl: '../images/44.png',
-    //     playUrl: 'https://play.grwth.hk/play/?roomId=q0sVfMyp5xQZNoCDzM6doXSJP9fbGr&userId=27cb7a613d99e87f7b9e176c7b0fa2b4'
+    //     playUrl: 'https://play.grwth.hk/preview/?roomId=q0sVfMyp5xQZNoCDzM6doXSJP9fbGr&userId=27cb7a613d99e87f7b9e176c7b0fa2b4'
     // },
     {
         imgUrl: '../images/45.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=ZfiQoqGs8ZhEXOmIzKsE6wscgDwvMh&userId=27cb7a613d99e87f8e0ac29d4027ced3'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=ZfiQoqGs8ZhEXOmIzKsE6wscgDwvMh&userId=27cb7a613d99e87f8e0ac29d4027ced3'
     },
     {
         imgUrl: '../images/46.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=wSEhIKgrK8JJ6zB9lE5fsSoMB3TEhZ&userId=27cb7a613d99e87f8e0ac29d4027ced3'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=wSEhIKgrK8JJ6zB9lE5fsSoMB3TEhZ&userId=27cb7a613d99e87f8e0ac29d4027ced3'
     },
     {
         imgUrl: '../images/47.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=EK34rGjqUXe176YqQMU7OLHYhuNxsS&userId=27cb7a613d99e87f99a4469c6a6ac5f0'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=EK34rGjqUXe176YqQMU7OLHYhuNxsS&userId=27cb7a613d99e87f99a4469c6a6ac5f0'
     },
     {
         imgUrl: '../images/48.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=z6FBNkKIGsa6vioFQbZq88psWVevWQ&userId=27cb7a613d99e87f99a4469c6a6ac5f0'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=z6FBNkKIGsa6vioFQbZq88psWVevWQ&userId=27cb7a613d99e87f99a4469c6a6ac5f0'
     },
     {
         imgUrl: '../images/49.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=c4hu8GeBomEuE2pO88nTmSMegPZSCb&userId=27cb7a613d99e87fbee0fd3866ff914b'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=c4hu8GeBomEuE2pO88nTmSMegPZSCb&userId=27cb7a613d99e87fbee0fd3866ff914b'
     },
     {
         imgUrl: '../images/50.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=CvyfFyHoXOY4K2hQTS7aBMbRmgiXFg&userId=27cb7a613d99e87f4ce6922ad4dc7a52&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=CvyfFyHoXOY4K2hQTS7aBMbRmgiXFg&userId=27cb7a613d99e87f4ce6922ad4dc7a52&assignmentId=undefined'
     },
     {
         imgUrl: '../images/51.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=YbzaNQjaf1l8pWDkPtYpI9U0FZ9GEZ&userId=27cb7a613d99e87f4ce6922ad4dc7a52&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=YbzaNQjaf1l8pWDkPtYpI9U0FZ9GEZ&userId=27cb7a613d99e87f4ce6922ad4dc7a52&assignmentId=undefined'
     },
     {
         imgUrl: '../images/52.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=dpykNPUfTllMtfZLoG3NcV6YQCED0B&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=dpykNPUfTllMtfZLoG3NcV6YQCED0B&userId=27cb7a613d99e87f7b9e176c7b0fa2b4&assignmentId=undefined'
     },
     {
         imgUrl: '../images/53.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=T3vqt7CdYQV6Obr9XHteFewglv9V1Y&userId=27cb7a613d99e87f8e0ac29d4027ced3'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=T3vqt7CdYQV6Obr9XHteFewglv9V1Y&userId=27cb7a613d99e87f8e0ac29d4027ced3'
     },
     {
         imgUrl: '../images/54.png',
-        playUrl: 'https://play.grwth.hk/play/?roomId=T1oTzf5YqaSs7LMdVihDjgklcbGnRR&userId=27cb7a613d99e87f8e0ac29d4027ced3'
+        playUrl: 'https://play.grwth.hk/preview/?roomId=T1oTzf5YqaSs7LMdVihDjgklcbGnRR&userId=27cb7a613d99e87f8e0ac29d4027ced3'
     },
     // {
     //     imgUrl: '../images/big1.png',
-    //     playUrl: 'https://play.grwth.hk/play/?roomId=l1PH15b8GMfARzEvDqPQ2VeNHSRXNj&userId=27cb7a613d99e87f998183af8de6c415'
+    //     playUrl: 'https://play.grwth.hk/preview/?roomId=l1PH15b8GMfARzEvDqPQ2VeNHSRXNj&userId=27cb7a613d99e87f998183af8de6c415'
     // },
     // {
     //     imgUrl: '../images/big2.png',
-    //     playUrl: 'https://play.grwth.hk/play/?roomId=U5XFVECxPhNn509auVoVAWHm6Dzmko&userId=27cb7a613d99e87f998183af8de6c415'
+    //     playUrl: 'https://play.grwth.hk/preview/?roomId=U5XFVECxPhNn509auVoVAWHm6Dzmko&userId=27cb7a613d99e87f998183af8de6c415'
     // },
 
 ];
